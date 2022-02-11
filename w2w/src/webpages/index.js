508504ddaea2d9911116e091425380d46e93353f
AhmedYasser-gu <gusyasah@student.gu.se>
2022-02-11 19:28:32 +0100

    using fetch

--- w2w/src/webpages/index.js
+++ w2w/src/webpages/index.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
 
 const Home = () => {
-    return <div style={{
-        display: 'flex', justifyContent:
-            'center', alignItems: 'center', height: '90vh', background: '#4D4847'
-    }}>
-        <h1>Welcome to our Homepage ! </h1>
-    </div>;
-};
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users/")
+            .then(res => res.json())
+            .then(
+                (data) => {
+                    setIsLoaded(true);
+                    setUsers(data);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+      }, [])
+
+if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <ul>
+                {users.map(user => (
+                <li key={user.id}>
+                    {user.name} 
+                </li>
+                ))}
+            </ul>
+        );
+    }
+}
 
 export default Home;
