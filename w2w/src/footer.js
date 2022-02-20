afcfe7d0867a84a6f22454d20b376ccb4f2130ee
AhmedYasser-gu <gusyasah@student.gu.se>
2022-02-20 17:27:47 +0100

    css updates

--- w2w/src/footer.js
+++ w2w/src/footer.js
@@ -1,14 +1,15 @@
 import React from "react";
+import './App.css';
 
 var style = {
-    backgroundColor: "#1C3738",
+    backgroundColor: "#4D4847",
     borderTop: "0px solid #1C3738",
     textAlign: "center",
-    padding: "35px",
+    padding: "50px",
     position: "fixed",
     left: "0",
     bottom: "0",
-    height: "65px",
+    height: "8%",
     width: "100%",
 };
 
@@ -17,12 +18,14 @@ function Footer() {
         <div style={style}>
      <strong>   
          <h4 style={{ color: '#F4FFF8' }}> Chad Coders™</h4>
-        <p style={{ color: '#F4FFF8' }}> Any kind of feedback or faced an issue ? 
-            Please feel free to contact us at anytime | 
-            Email : ahmedtaalab95@hotmail.com
+        <p style={{ color: '#F4FFF8' }}> Any kind of feedback ? 
+            Please feel free to contact us at anytime <br></br>
+         <a href="https://www.linkedin.com/in/ahmed-yasser-7b6a351b1/">
+         <img className="img"  src={require("./LinkedIn.png")} alt="LinkedIn"></img></a>
         </p>
-        </strong>    
+        </strong>   
     </div>
+    
     );
 }
 
