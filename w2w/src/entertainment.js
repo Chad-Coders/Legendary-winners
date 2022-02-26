1886e97cf0286b88add41d0f4b2b0258932e3d4a
AhmedYasser-gu <gusyasah@student.gu.se>
2022-02-26 12:55:50 +0100

    set up for ent. page

--- w2w/src/entertainment.js
+++ w2w/src/entertainment.js
@@ -5,7 +5,18 @@ const Entertainment = () => {
         display: 'flex', justifyContent:
             'center', alignItems: 'center', height: '90vh'
     }}>
-        <h1> TV Shows / Movies </h1>
+        <h1> TV Shows / Movies Details </h1>
+            
+        <div className='stream'>
+
+
+            <div className='movieDetails'>
+
+
+
+            </div>
+        </div>
+        
         
     </div>;
 };
