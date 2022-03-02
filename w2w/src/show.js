4d8d10073c5b7e06c2071b6b61d6b0bb11e6fc82
AxelBroberg <axellakris2@gmail.com>
2022-03-02 09:32:56 +0100

    Made only streamable show, added seasons figure

--- w2w/src/show.js
+++ w2w/src/show.js
@@ -28,18 +28,18 @@ export default class Item extends React.Component {
 
     render() {
         console.log("Offers:")
-        console.log(this.state.offers)
+        console.log(this.state.seasons)
         //TMDB AND IMDB SCORE VARIABLES, EXTRACTIONS
-        var tmdbscore,imdbscore,imdbvotes;
+        var tmdbscore, imdbscore, imdbvotes;
         for (let index = 0; index < this.state.scoring.length; index++) {
             const element = this.state.scoring[index];
-            if(element.provider_type === "tmdb:score"){
-                tmdbscore =element.value;
+            if (element.provider_type === "tmdb:score") {
+                tmdbscore = element.value;
             }
-            if(element.provider_type === "imdb:score"){
+            if (element.provider_type === "imdb:score") {
                 imdbscore = element.value;
             }
-            if(element.provider_type === "imdb:votes"){
+            if (element.provider_type === "imdb:votes") {
                 imdbvotes = element.value;
             }
         }
@@ -49,6 +49,7 @@ export default class Item extends React.Component {
                     <h6>{reactStringReplace(this.state.item.poster, /(\d+)/g, (match, i) =>
                         <img key={i} src={`https://images.justwatch.com/poster/${match}/s592`}
                             alt={this.state.item.title}></img>
+
                     )}</h6>
                     <h1>{this.state.item.title}</h1>
 
@@ -60,21 +61,40 @@ export default class Item extends React.Component {
                     <h3>Can be streamed at</h3>
                     <figure className="provBox">
                         {this.state.offers.map(function (prov, i) {
-                            return <div class="boxDiv">
-                                <p key={i} class="provBox2">
-                                    <a href={prov.urls.standard_web}><img src={require(`./img/${prov.package_short_name}.png`)} alt={prov.package_short_name}>
-                                    </img>
-                                    </a>
-                                    <div class="presType">
-                                        {prov.presentation_type}
-                                    </div>
-                                </p>
-                            </div>
+
+                            if (prov.monetization_type === 'flatrate') {
+                                return <div class="boxDiv">
+                                    <p key={i} class="provBox2">
+                                        <a href={prov.urls.standard_web}><img src={require(`./img/${prov.package_short_name}.png`)} alt={prov.package_short_name}>
+                                        </img>
+                                        </a>
+                                        <div class="presType">
+                                            {prov.presentation_type}
+                                        </div>
+                                    </p>
+                                </div>
+                            } else {
+                                return <div></div>
+                            }
+
                         })}
                     </figure>
                 </figure>
 
                 <br></br>
+
+                <figure className="results">
+                    <h3>Seasons</h3>
+                    <div>
+                        {this.state.seasons.map(function (prov, i) {
+                            return <div key={i}>
+                                <h5>
+                                    {prov.title} { }
+                                </h5>
+                            </div>
+                        })}
+                    </div>
+                </figure>
 
                 <figure className="resultsText">
                     <h3>IMDB rating: {imdbscore} ({imdbvotes}) &emsp; &emsp; TMDB rating: {tmdbscore}  </h3>
